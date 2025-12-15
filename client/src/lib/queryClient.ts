9f1972cfb73449d99717631745614e4c7e03e120
cubastionfrontend1645 <cubastion.1234@gmail.com>
2025-12-15 21:30:35 +0530

    Revert "new code +++"
    This reverts commit 8036c8793ce4508722b526448d9acb97f7d84400.

--- client/src/lib/queryClient.ts
+++ client/src/lib/queryClient.ts
@@ -1,9 +1,5 @@
 import { QueryClient, QueryFunction } from "@tanstack/react-query";
 
-<<<<<<< HEAD
-import { resolveApi } from "./api";
-=======
->>>>>>> parent of 8036c87 (new code +++)
 async function throwIfResNotOk(res: Response) {
   if (!res.ok) {
     const text = (await res.text()) || res.statusText;
@@ -14,15 +10,9 @@ async function throwIfResNotOk(res: Response) {
 export async function apiRequest(
   method: string,
   url: string,
-<<<<<<< HEAD
-  data?: unknown
-): Promise<Response> {
-  const res = await fetch(resolveApi(url), {
-=======
   data?: unknown | undefined,
 ): Promise<Response> {
   const res = await fetch(url, {
->>>>>>> parent of 8036c87 (new code +++)
     method,
     headers: data ? { "Content-Type": "application/json" } : {},
     body: data ? JSON.stringify(data) : undefined,
@@ -33,12 +23,6 @@ export async function apiRequest(
   return res;
 }
 
-<<<<<<< HEAD
-
-// ------------------------------------------------------------
-// Default Query FN for React Query
-// ------------------------------------------------------------
-
 =======
 >>>>>>> parent of 8036c87 (new code +++)
 type UnauthorizedBehavior = "returnNull" | "throw";
@@ -47,14 +31,9 @@ export const getQueryFn: <T>(options: {
 }) => QueryFunction<T> =
   ({ on401 }) =>
   async ({ queryKey }) => {
-<<<<<<< HEAD
-    const url = resolveApi(queryKey.join("/"));
-    const res = await fetch(url, { credentials: "include" });
-=======
     const res = await fetch(queryKey.join("/") as string, {
       credentials: "include",
     });
->>>>>>> parent of 8036c87 (new code +++)
 
     if (on401 === "returnNull" && res.status === 401) {
       return null;
@@ -64,14 +43,6 @@ export const getQueryFn: <T>(options: {
     return res.json();
   };
 
-<<<<<<< HEAD
-
-// ------------------------------------------------------------
-// React Query Client
-// ------------------------------------------------------------
-
-=======
->>>>>>> parent of 8036c87 (new code +++)
 export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
